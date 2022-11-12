54b3771bf6ad2358c66905962e4490e9ca5d9dfa
Virginia Álvarez <virginiaalvarez.ar@gmail.com>
2022-11-12 20:40:38 +0100

    add dom in function render

--- src/js/main.js
+++ src/js/main.js
@@ -1,53 +1,100 @@
 'use strict';
-// Declaración de Variables
-    //queryselector
+// DECLARACIÓN DE VARIABLES
+//queryselector
+    
 const charactersList = document.querySelector('.jsCharactersList');
 const character = document.querySelector('.jsCharacter');
 const input = document.querySelector('.jsInput');
 const btn = document.querySelector('.jsBtn');
-    //Variables globales 
+const favouritesList= document.querySelector('.jsFavouritesList');
+
+//Variables globales 
 let characters = [];
 let favouritesCharacters = [];
 
-// Declaración de Funciones
+// DECLARACIÓN DE FUNCIONES
 
-    //función renderizar
+//función renderizar
 function drawCharacter(character){
     //Aquí código para pintar
-    charactersList.innerHTML += `<li class="list jsCharacter">
-    <img class="list-img" src="${character.img}" alt="Foto ${character.name}">
-    <p class="list-paragraph">${character.name}</p>
-    <p class="list-paragraph">${character.status}</p>
-</li>`  
+    const liElement = document.createElement('li');
+    liElement.classList.add('list');
+    liElement.classList.add('jsCharacter');
+    
+    const imgElement = document.createElement('img');
+    imgElement.setAttribute('src',character.img);
+    imgElement.setAttribute('alt',character.name);
+    imgElement.classList.add('list-img');
+    liElement.appendChild(imgElement);
+    
+    const paragraph1Element = document.createElement('p');
+    const paragraph1Name = document.createTextNode(character.name);
+    paragraph1Element.appendChild(paragraph1Name);
+    paragraph1Element.classList.add('list-paragraph');
+    liElement.appendChild(paragraph1Element);
+    
+    const paragraph2Element = document.createElement('p');
+    const paragraph2Name = document.createTextNode(character.status);
+    paragraph2Element.appendChild(paragraph2Name)
+    paragraph2Element.classList.add('list-paragraph');
+    liElement.appendChild(paragraph2Element);
+
+    // liElement.addEventListener('click', addToFavourite)
+
+    charactersList.appendChild(liElement);
+
+
+
+
+//     charactersList.innerHTML += `<li class="list jsCharacter">
+//     <img class="list-img" src="${character.img}" alt="Foto ${character.name}">
+//     <p class="list-paragraph">${character.name}</p>
+//     <p class="list-paragraph">${character.status}</p>
+// </li>`  
 };
 
-    //función filtrar
+//función filtrar
 function filterCharacter (event){
     event.preventDefault();
     const nameSearch= input.value;
     charactersList.innerHTML = '';
-    const filteredCharacters = characters.filter((character) =>
-    character.name.toLowerCase().includes(nameSearch.toLowerCase()));
-    console.log(filterCharacter);
-    for(const character of filteredCharacters){
-        drawCharacter(character);
-    }
+    // const filteredCharacters = characters.filter((character) =>
+    // character.name.toLowerCase().includes(nameSearch.toLowerCase()));
+    // console.log(filterCharacter);
+    // for(const character of filteredCharacters){
+    //     drawCharacter(character);
+    // }
+    fetchFilteredCharacters(nameSearch);
+}
+//función filtrar por api
+function fetchFilteredCharacters(name){
+    fetch(`https://breakingbadapi.com/api/characters?name=${name}`)
+    .then((response) => response.json())
+    .then((filteredCharacters) => {
+            for(const character of filteredCharacters){
+                drawCharacter(character);
+            }
+    }); 
+}
+// función petición al servidor
+function fetchAllCharacters(){
+    fetch('https://breakingbadapi.com/api/characters')
+    .then((response) => response.json())
+    .then((jsonData) => {
+            characters = jsonData;
+            localStorage.setItem('characters', characters)
+            for(const character of characters){
+                drawCharacter(character);
+            }
+    }); 
 }
 
 //Eventos
 btn.addEventListener('click', filterCharacter);
-//Ejecucciones: codigo que se ejecuta al cargar la pagina
 
-//servidor
-fetch('https://breakingbadapi.com/api/characters')
-  .then((response) => response.json())
-  .then((jsonData) => {
-        characters = jsonData;
-        localStorage.setItem('characters', characters)
-        for(const character of characters){
-            drawCharacter(character);
-        }
-});
+
+//Ejecucciones: 
+fetchAllCharacters();
 
 
 
