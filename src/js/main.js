4406267ab3e5a83ced4dba0bf6ba65378ff4b8de
Virginia Álvarez <virginiaalvarez.ar@gmail.com>
2022-11-12 13:46:21 +0100

    add basic css and first part of js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,43 @@
 'use strict';
+// Declaración de Variables
+//queryselector
+const charactersList = document.querySelector('.jsCharactersList');
+const character = document.querySelector('.jsCharacter');
+//Variables globales 
+let characters; // = a lo que recuperes del fetch
+
+// Declración de Funciones
+
+//función renderizar
+function drawCharacter(character){
+    //Aquí código para pintar
+    charactersList.innerHTML += `<li class="list jsCharacter">
+    <img class="list-img" src="${character.img}" alt="Foto ${character.name}">
+    <p class="list-paragraph">${character.name}</p>
+    <p class="list-paragraph">${character.status}</p>
+</li>`
+};
+
+//Ejecucciones: codigo que se ejecuta al cargar la pagina
+
+//servidor
+fetch('https://breakingbadapi.com/api/characters')
+  .then((response) => response.json())
+  .then((jsonData) => {
+        characters = jsonData;
+        localStorage.setItem('characters', characters)
+        for(const character of characters){
+            drawCharacter(character);
+        }
+});
+
+
+
+
+
+
+
+
+
 
 
