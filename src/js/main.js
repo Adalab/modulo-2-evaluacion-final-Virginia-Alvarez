526bae25b1a2bd1e6ce44213358e44ba82d2ee47
Virginia Álvarez <virginiaalvarez.ar@gmail.com>
2022-11-14 11:26:53 +0100

    add changes in js and css

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 'use strict';
-// DECLARACIÓN DE VARIABLES
+// 1-DECLARACIÓN DE VARIABLES
 //queryselector
     
 const charactersList = document.querySelector('.jsCharactersList');
@@ -7,6 +7,11 @@ const character = document.querySelector('.jsCharacter');
 const input = document.querySelector('.jsInput');
 const btn = document.querySelector('.jsBtn');
 const favouritesList= document.querySelector('.jsFavouritesList');
+
+const titleCharacters= document.querySelector('.jsTitleCharacters');
+const titleFavourites= document.querySelector('.jsTitleFav');
+const articleCharacters= document.querySelector('.jsArticleCharacters');
+const articleFavourites= document.querySelector('.jsFavouritesList');
 
 //Variables globales 
 let characters = [];
@@ -20,9 +25,11 @@ function addToFavourite(event){
     event.preventDefault();
     const id = parseInt(event.currentTarget.dataset.id);
     const selectedCharacter = characters.find((character) => character.char_id === id);
-    favouritesCharacters.push(selectedCharacter);
-    localStorage.setItem('favouritesCharacters', JSON.stringify(favouritesCharacters));
-    drawCharacter(selectedCharacter, favouritesList, true);
+    if(!favouritesCharacters.find((character) => character.char_id === id)){ //Para evitar que se me añada varias veces el mismo favorito
+        favouritesCharacters.push(selectedCharacter);
+        localStorage.setItem('favouritesCharacters', JSON.stringify(favouritesCharacters));
+        drawCharacter(selectedCharacter, favouritesList, true);
+    } 
 }
 
 function removeFromFavourite(event){
@@ -33,45 +40,40 @@ function removeFromFavourite(event){
     event.currentTarget.remove();
 }
 
-//función renderizar
+//3-función renderizar
 function drawCharacter(character, list, isFavourite){
-    //Le paso dos parámetros (personaje, lista donde va a ir guardada), para poder reutilizarla en fav y en la busqueda.
+    //sólo me pinta un personaje. en la función fetch con el bucle pinto todos.
+    //Le paso parámetros (personaje, lista donde va a ir guardada,si es fav), para poder reutilizarla en fav y en la busqueda.
     const liElement = document.createElement('li');
-    liElement.classList.add('list');
+    liElement.classList.add('character');
     liElement.classList.add('jsCharacter');
-    if(isFavourite){
-        liElement.classList.add('favourite');
-    }
     liElement.dataset.id = character.char_id; //Añado id del personaje en el atributo data-id
     
     const imgElement = document.createElement('img');
     imgElement.setAttribute('src',character.img);
     imgElement.setAttribute('alt',character.name);
-    imgElement.classList.add('list-img');
+    imgElement.classList.add('character-img');
     liElement.appendChild(imgElement);
     
     const paragraph1Element = document.createElement('p');
     const paragraph1Name = document.createTextNode(character.name);
     paragraph1Element.appendChild(paragraph1Name);
-    paragraph1Element.classList.add('list-paragraph');
+    paragraph1Element.classList.add('character-paragraph');
     liElement.appendChild(paragraph1Element);
     
     const paragraph2Element = document.createElement('p');
     const paragraph2Name = document.createTextNode(character.status);
     paragraph2Element.appendChild(paragraph2Name)
-    paragraph2Element.classList.add('list-paragraph');
+    paragraph2Element.classList.add('character-paragraph');
     liElement.appendChild(paragraph2Element);
 
-    if(!isFavourite) {
-        liElement.addEventListener('click', addToFavourite);
-    } else {
+    if(isFavourite){
+        liElement.classList.add('favourite');
         liElement.addEventListener('click', removeFromFavourite);
+    } else {
+        liElement.addEventListener('click', addToFavourite);
     }
-
-    list.appendChild(liElement);
-
-
-
+    list.appendChild(liElement);// a la lista que yo le digo, pintame el li
 
 //     charactersList.innerHTML += `<li class="list jsCharacter">
 //     <img class="list-img" src="${character.img}" alt="Foto ${character.name}">
@@ -80,7 +82,7 @@ function drawCharacter(character, list, isFavourite){
 // </li>`  
 };
 
-//función filtrar
+//4-función filtrar
 function filterCharacter (event){
     event.preventDefault();
     const nameSearch= input.value;
@@ -93,7 +95,7 @@ function filterCharacter (event){
     // }
     fetchFilteredCharacters(nameSearch);
 }
-//función filtrar por api
+//5-función filtrar por api
 function fetchFilteredCharacters(name){
     fetch(`https://breakingbadapi.com/api/characters?name=${name}`)
     .then((response) => response.json())
@@ -103,25 +105,24 @@ function fetchFilteredCharacters(name){
             }
     }); 
 }
-// función petición al servidor
+//2- función petición al servidor
 function fetchAllCharacters(){
     fetch('https://breakingbadapi.com/api/characters')
     .then((response) => response.json())
     .then((jsonData) => {
             characters = jsonData;
-            localStorage.setItem('characters', JSON.stringify(characters));
             for(const character of characters){
                 drawCharacter(character, charactersList, false);
             }
     }); 
-}
+};
 
 function drawFavouritesCharacters(){
-    if(favouritesCharacters){
+    if(favouritesCharacters.length){
         for(const character of favouritesCharacters){
             drawCharacter(character, favouritesList, true);
         }
-    }
+    }   
 };
 
 //Eventos
